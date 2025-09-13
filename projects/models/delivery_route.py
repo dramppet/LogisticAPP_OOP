24c0f45e7d3db843356d5f528118ecd93c72053a
DragomirPetko <dramppet@abv.bg>
2025-09-13 23:22:44 +0300

    models and exception

--- projects/models/delivery_route.py
+++ projects/models/delivery_route.py
@@ -1,9 +1,60 @@
+from typing import Optional, List,Tuple
+from datetime import datetime
+from functools import reduce
+
+from models.delivery_package import DeliveryPackage
+from exception.not_valid_route_location import NotValidRouteLocationExseption
+from models.truck import Truck
+from map.map import Map
+
 class DeliveryRoute:
     _next_id = 1
-    def __init__(self,start_location:str, end_location: str, *location) -> None:
+    
+    def __init__(self, *location: str, deperture_time: Optional[datetime] = None) -> None:
         self.route_id = DeliveryRoute._next_id
         DeliveryRoute._next_id +=1
-        self.start_location = start_location
-        self.end_location = end_location
-        self.location = []
+                       
+        self.location = tuple(location)
+        self.departure_time = deperture_time or deperture_time.now()
         
+        self.truck: Optional[Truck]  = None
+        self.packacges: List[DeliveryPackage]  = []
+        
+    @property
+    def locations(self) -> Tuple[str,...]: 
+        return Tuple(self._locations)   
+
+    @location.setter
+    def location(self, value: Tuple[str,...]):
+        if len(value) < 2:
+            raise NotValidRouteLocationExseption('Location must be more 1 distination')
+        
+        normalized = tuple(v.upper() for v in value)
+        
+        for el in normalized:
+            if not Map._valid_location(el):
+                raise NotValidRouteLocationExseption(f'Invalid location {el}')
+        
+        self._location = normalized
+        
+    @property
+    def total_distance(self):
+        segments = zip(self._location,self._location[1:])
+        return reduce(lambda total,second : total + Map.calculate_distance(*second),
+                      segments,0)
+        
+    @property
+    def finish_route(self):
+        return self.total_distance() - Truck._MAX_SPEED
+        
+    def info(self)->str:
+        return {
+            f'Delivery Route {self._next_id}',
+            f'Start location {self._location[0]}'
+            f'End location {self._location[-1]}'
+            f'Create route {self.departure_time}'
+            f'End route {self.finish_route()}'
+            f'Truck {Truck.name}'
+        }
+
+    
--- projects/models/truck.py
+++ projects/models/truck.py
@@ -1,6 +1,7 @@
 from status.truck import TruckStatus
 
 class Truck:
+    _MAX_SPEED  = 87
     def __init__(self,vehicle_id: int, name: str, capacity: int, max_range: int) -> None:
         self.vehicle_id = vehicle_id
         self.name = name
@@ -9,6 +10,17 @@ class Truck:
         self.status  = TruckStatus.FREE
         self.route = None
         self.location = None
+        
+        
+    def info(self) -> str:
+       return (
+            f"Vehicle_id: {self.vehicle_id}\n"
+            f"Name: {self.name}\n"
+            f"Capacity: {self.capacity}\n"
+            f"Max range: {self.max_range}\n"
+            f"Status: {self.status}\n"
+            f"Location: {self.current_location or 'Unknown'}"
+        )
         
         
         
